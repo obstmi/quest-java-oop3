5668d6c369f9162f56fe3211904f33cc2d16e382
Michael Obst <michael.obst@telekom.de>
2019-11-26 16:29:24 +0100

    Loesung Quest 8

--- Eagle.java
+++ Eagle.java
@@ -1,24 +1,73 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
-    private boolean flying;
-    private int altitude;
+	private boolean flying;
+	private int altitude;
 
-    public Eagle(String name) {
-        super(name);
-        this.flying = false;
-        this.altitude = 0;
-    }
+	public Eagle(String name) {
+		super(name);
+		this.flying = false;
+		this.altitude = 0;
+	}
 
-    public int getAltitude() {
-        return altitude;
-    }
+	@Override
+	public int ascend(int meters) {
+		if (!this.isFlying()) {
+			System.out.println(this.getName() + " can't ascend. "+this.getName()+" is not flying.");
+		} else {
+			System.out.print(this.getName() + " ascends " + meters + " meters.");	
+			this.altitude+=meters;
+			System.out.println(" New altitude: "+this.getAltitude());
+		}
+		return this.getAltitude();
+	}
 
-    public boolean isFlying() {
-        return flying;
-    }
+	@Override
+	public int descend(int meters) {
+		if (!this.isFlying()) {
+			System.out.println(this.getName() + " can't descend. "+this.getName()+" is not flying.");
+		} else if (this.getAltitude() <= meters) {
+			System.out.println(this.getName() + " descends " + this.getAltitude() + " meters.");
+			this.altitude = 0;
+			this.land();
+		} else {
+			System.out.print(this.getName() + " descends " + meters + " meters.");	
+			this.altitude-=meters;
+			System.out.println(" New altitude: "+this.getAltitude());
+		}
 
-    @Override
-    public String sing() {
-        return "Screech!";
-    }
+		return this.getAltitude();
+	}
+
+	@Override
+	public void land() {
+		if (!this.isFlying()) {
+			System.out.println(this.getName() + " is not flying.");
+		} else {
+			System.out.println(this.getName() + " is landing!!");
+			this.flying = false;
+		}
+	}
+
+	@Override
+	public void takeOff() {
+		if (this.isFlying()) {
+			System.out.println(this.getName() + " is already flying.");
+		} else {
+			this.flying = true;
+			System.out.println(this.getName() + " takes off :)))");
+		}
+	}
+
+	public int getAltitude() {
+		return altitude;
+	}
+
+	public boolean isFlying() {
+		return flying;
+	}
+
+	@Override
+	public String sing() {
+		return "Screech!";
+	}
 }
